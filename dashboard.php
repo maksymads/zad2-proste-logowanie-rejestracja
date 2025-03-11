e2db049ffda1147d340dffb96049a6da60066049
maksymads <147048960+maksymads@users.noreply.github.com>
2025-03-11 09:55:20 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -16,7 +16,6 @@ if (!isset($_SESSION['user_id'])) {
 <body>
     <div class="container">
         <h1>Witaj, <?= $_SESSION['username'] ?></h1>
-        <p>To jest Twój panel użytkownika.</p>
         <p><a href="logout.php" class="btn">Wyloguj się</a></p>
     </div>
 </body>
